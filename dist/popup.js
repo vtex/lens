dbdd28c10894ac793d5eddeaddae15b1aea8ef64
Guilherme Berger <guilherme.berger@gmail.com>
2013-11-01 14:51:03 -0200

    Better detection of siteName. Wrap all JS on chrome.tabs.query

--- dist/popup.js
+++ dist/popup.js
@@ -1,90 +1,97 @@
 (function() {
-  var changeVtexEnv, getTab, refresh;
-
-  getTab = function(callback) {
+  $(document).ready(function() {
     return chrome.tabs.query({
       active: true,
       currentWindow: true
     }, function(tabs) {
-      return callback(tabs[0]);
-    });
-  };
-
-  changeVtexEnv = function(env, callback) {
-    return getTab(function(tab) {
-      var a, parts, siteName, url;
+      var a, changeVtexEnv, getSiteName, refresh, tab, updateSiteName;
+      tab = tabs[0];
       a = document.createElement("a");
       a.href = tab.url;
-      if (/vtexcommerce/.test(a.hostname)) {
-        parts = a.hostname.split(".");
-        siteName = (parts[0] === "www" ? parts[1] : parts[0]);
-      } else if (jsnomeSite) {
-        siteName = jsnomeSite;
-      } else {
-        siteName = "UNKNOWN";
-      }
-      url = a.protocol + "//" + siteName + "." + env + ".com.br" + a.pathname + a.search + a.hash;
-      chrome.tabs.update(tab.id, {
-        url: url
-      });
-      return callback();
-    });
-  };
-
-  $(".env-change").on("click", function() {
-    return changeVtexEnv($(this).data("env"), function() {
-      return window.close();
-    });
-  });
-
-  refresh = function() {
-    return getTab(function(tab) {
-      return chrome.cookies.getAll({
-        url: tab.url
-      }, function(cookies) {
-        var cookiesObj, section;
-        section = $('#cookies');
-        cookiesObj = {};
-        cookies.forEach(function(c) {
-          return cookiesObj[c.name] = c;
-        });
-        return section.find('li').each(function(i, el) {
-          var id, name, status, _ref;
-          name = $(el).data('cookieName');
-          id = $(el).attr('id');
-          status = section.find('#' + id).find('.status').removeClass('enabled disabled unknown');
-          switch ((_ref = cookiesObj[name]) != null ? _ref.value : void 0) {
-            case 0:
-            case "0":
-            case "Value=0":
-              return status.text('enabled').addClass('enabled');
-            case 1:
-            case "1":
-            case "Value=1":
-              return status.text('disabled').addClass('disabled');
-            default:
-              return status.text('unknown').addClass('unknown');
+      updateSiteName = function(siteName) {
+        var parts;
+        if (siteName == null) {
+          siteName = null;
+        }
+        if (siteName === null) {
+          if (/vtexcommerce/.test(a.hostname)) {
+            parts = a.hostname.split('.');
+            siteName = (parts[0] === "www" ? parts[1] : parts[0]);
+          } else if (typeof jsnomeSite !== "undefined" && jsnomeSite !== null) {
+            siteName = jsnomeSite;
+          } else {
+            parts = a.hostname.split('.');
+            siteName = (parts[0] === "www" ? parts[1] : parts[0]);
           }
+        }
+        return $('#env').data('siteName', siteName);
+      };
+      getSiteName = function() {
+        return $("#env").data('siteName');
+      };
+      changeVtexEnv = function(env, callback) {
+        var siteName, url;
+        siteName = getSiteName();
+        url = a.protocol + "//" + siteName + "." + env + ".com.br" + a.pathname + a.search + a.hash;
+        chrome.tabs.update(tab.id, {
+          url: url
         });
+        return callback();
+      };
+      refresh = function() {
+        var _ref;
+        if ((_ref = getSiteName()) === '' || _ref === null || _ref === (void 0)) {
+          $('#env a').addClass('pure-button-disabled');
+        } else {
+          $('#env a').removeClass('pure-button-disabled');
+        }
+        return chrome.cookies.getAll({
+          url: tab.url
+        }, function(cookies) {
+          var cookiesObj, section;
+          section = $('#cookies');
+          cookiesObj = {};
+          cookies.forEach(function(c) {
+            return cookiesObj[c.name] = c;
+          });
+          return section.find('li').each(function(i, el) {
+            var id, name, status, _ref1;
+            name = $(el).data('cookieName');
+            id = $(el).attr('id');
+            status = section.find('#' + id).find('.status').removeClass('enabled disabled unknown');
+            switch ((_ref1 = cookiesObj[name]) != null ? _ref1.value : void 0) {
+              case 1:
+              case "1":
+              case "Value=1":
+                return status.text('disabled').addClass('disabled');
+              default:
+                return status.text('enabled').addClass('enabled');
+            }
+          });
+        });
+      };
+      $(".env-change").on("click", function() {
+        if (!$(this).hasClass('pure-button-disabled')) {
+          return changeVtexEnv($(this).data("env"), function() {
+            return window.close();
+          });
+        }
       });
-    });
-  };
-
-  $('#cookies .action').on('click', function() {
-    var name, value;
-    value = $(this).hasClass('enable') ? 'Value=0' : 'Value=1';
-    name = $(this).closest('.cookie').data('cookieName');
-    return getTab(function(tab) {
-      chrome.cookies.set({
-        url: tab.url,
-        name: name,
-        value: value,
-        expirationDate: moment().add('days', 7).unix()
+      $('#cookies .action').on('click', function() {
+        var name, value;
+        value = $(this).hasClass('enable') ? 'Value=0' : 'Value=1';
+        name = $(this).closest('.cookie').data('cookieName');
+        chrome.cookies.set({
+          url: tab.url,
+          name: name,
+          value: value,
+          expirationDate: moment().add('days', 7).unix()
+        });
+        return refresh();
       });
+      updateSiteName();
       return refresh();
     });
   });
-
-  $(document).ready(refresh);
 
 }).call(this);
--- src/popup.coffee
+++ src/popup.coffee
@@ -1,55 +1,72 @@
-getTab = (callback) ->
+$(document).ready ->
 	chrome.tabs.query {active: true, currentWindow: true}, (tabs) ->
-		callback tabs[0]
-
-changeVtexEnv = (env, callback) ->
-	getTab (tab) ->
+		tab = tabs[0]
 		a = document.createElement("a")
 		a.href = tab.url
-		if /vtexcommerce/.test(a.hostname)
-			parts = a.hostname.split(".")
-			siteName = (if parts[0] is "www" then parts[1] else parts[0])
-		else if jsnomeSite
-			siteName = jsnomeSite
-		else
-			siteName = "UNKNOWN"
-		url = a.protocol + "//" + siteName + "." + env + ".com.br" + a.pathname + a.search + a.hash
-		chrome.tabs.update tab.id, url: url
 
-		callback()
+		updateSiteName = (siteName = null) ->
 
+			if siteName is null
+				if /vtexcommerce/.test(a.hostname)
+					parts = a.hostname.split('.')
+					siteName = (if parts[0] is "www" then parts[1] else parts[0])
+				else if jsnomeSite?
+					siteName = jsnomeSite
+				else
+					parts = a.hostname.split('.')
+					siteName = (if parts[0] is "www" then parts[1] else parts[0])
+#				else
+#					alert('Site desconhecido.')
+#					$('#env a').addClass('pure-button-disabled')
+#					callback()
+#					return
 
-$(".env-change").on "click", ->
-	changeVtexEnv $(this).data("env"), ->
-		window.close()
+			$('#env').data('siteName', siteName)
 
-refresh = ->
-	getTab (tab) ->
-		chrome.cookies.getAll {url: tab.url}, (cookies) ->
-			section = $('#cookies')
+		getSiteName = ->
+			$("#env").data('siteName')
 
-			cookiesObj = {}
-			cookies.forEach (c) ->
-				cookiesObj[c.name] = c
+		changeVtexEnv = (env, callback) ->
+			siteName = getSiteName()
 
-			section.find('li').each (i, el) ->
-				name = $(el).data('cookieName')
-				id = $(el).attr('id')
-				status = section.find('#' + id).find('.status').removeClass('enabled disabled unknown')
-				switch cookiesObj[name]?.value
-					when 0, "0", "Value=0"
-						status.text('enabled').addClass('enabled')
-					when 1, "1", "Value=1"
-						status.text('disabled').addClass('disabled')
-					else
-						status.text('unknown').addClass('unknown')
+			url = a.protocol + "//" + siteName + "." + env + ".com.br" + a.pathname + a.search + a.hash
+			chrome.tabs.update tab.id, url: url
 
-$('#cookies .action').on 'click', ->
-	value = if $(this).hasClass('enable') then 'Value=0' else 'Value=1'
-	name = $(this).closest('.cookie').data('cookieName')
-	getTab (tab) ->
-		chrome.cookies.set {url: tab.url, name: name, value: value, expirationDate: moment().add('days', 7).unix()}
+			callback()
+
+		refresh = ->
+			if getSiteName() in ['', null, undefined]
+				$('#env a').addClass('pure-button-disabled')
+			else
+				$('#env a').removeClass('pure-button-disabled')
+
+			chrome.cookies.getAll {url: tab.url}, (cookies) ->
+				section = $('#cookies')
+
+				cookiesObj = {}
+				cookies.forEach (c) ->
+					cookiesObj[c.name] = c
+
+				section.find('li').each (i, el) ->
+					name = $(el).data('cookieName')
+					id = $(el).attr('id')
+					status = section.find('#' + id).find('.status').removeClass('enabled disabled unknown')
+					switch cookiesObj[name]?.value
+						when 1, "1", "Value=1"
+							status.text('disabled').addClass('disabled')
+						else #when 0, "0", "Value=0"
+							status.text('enabled').addClass('enabled')
+
+		$(".env-change").on "click", ->
+			unless $(this).hasClass('pure-button-disabled')
+				changeVtexEnv $(this).data("env"), ->
+					window.close()
+
+		$('#cookies .action').on 'click', ->
+			value = if $(this).hasClass('enable') then 'Value=0' else 'Value=1'
+			name = $(this).closest('.cookie').data('cookieName')
+			chrome.cookies.set {url: tab.url, name: name, value: value, expirationDate: moment().add('days', 7).unix()}
+			refresh()
+
+		updateSiteName()
 		refresh()
-
-
-$(document).ready refresh
